6f71c8eb929c2fa883311b0477592411294de4d9
kokatoo <kokatoo1948@gmail.com>
2013-08-08 12:04:36 -0700

    Add matlab example.

--- returns.m
+++ returns.m
@@ -1,5 +1,6 @@
 function returns()
     compound30Years();
+    bondsStocks();
 end
 
 function compound30Years()
@@ -18,6 +19,42 @@ function compound30Years()
     fprintf('\nInitial amount: %d', initial);
     fprintf('\nMean return: %.2f', mu);
     fprintf('\nStd Dev: %.2f', sigma);
+    fprintf('\nFinal Mean: %.2f', mean(finalReturns));
+    fprintf('\nFinal Std Dev: %.2f', mean(finalReturns));
+    fprintf('\n\n');
+end
+
+function bondsStocks()
+    mu = [.085, .03];
+    correlation = -.05;
+    N = 5000;
+    weights = [.5, .5];
+    initial = 1000;
+    
+    sigma = [mu(1)^2, correlation*prod(mu); correlation*prod(mu), mu(2)^2];
+    
+    stockReturns = ones(N, 1);
+    bondReturns = ones(N, 1);
+    
+    for i = 1:30
+        scenarios = mvnrnd(mu, sigma, N);
+        stockReturns = stockReturns.*(1+scenarios(:, 1));
+        bondReturns = bondReturns.*(1+scenarios(:, 2));
+    end
+    
+    finalReturns = initial.*(weights(1).*stockReturns + weights(2).*bondReturns);
+    hist(finalReturns, 20);
+    title('Stocks And Bonds');
+    xlabel('30-Year Value');
+    ylabel('Counts');
+    
+    fprintf('\nInitial amount: %d', initial);
+    fprintf('\nMean return:\n');
+    disp(mu);
+    fprintf('\nCovariance Matrix:\n');
+    disp(sigma);
+    fprintf('\nWeights:\n');
+    disp(weights);
     fprintf('\nFinal Mean: %.2f', mean(finalReturns));
     fprintf('\nFinal Std Dev: %.2f', mean(finalReturns));
     fprintf('\n\n');
