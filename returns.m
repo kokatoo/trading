9d89e3b1763791dc0a9441bc3dc20e94f546a105
kokatoo <kokatoo1948@gmail.com>
2013-08-08 11:48:10 -0700

    Make naming consistent.

--- returns.m
+++ returns.m
@@ -8,17 +8,17 @@ function compound30Years()
     initial = 1000;
     
     rets = normrnd(mu, sigma, 100, 30);
-    values = initial * prod(1+rets, 2);
+    finalReturns = initial * prod(1+rets, 2);
 
-    hist(values, 20)
+    hist(finalReturns, 20)
     title('30-Year Compounding');
     xlabel('30-Year Value');
     ylabel('Counts');
     
-    fprintf('\nInitial capital: %d', initial);
+    fprintf('\nInitial amount: %d', initial);
     fprintf('\nMean return: %.2f', mu);
     fprintf('\nStd Dev: %.2f', sigma);
-    fprintf('\n30-Year Value Mean: %.2f', mean(values));
-    fprintf('\n30-Year Value Std Dev: %.2f', mean(values));
+    fprintf('\nFinal Mean: %.2f', mean(finalReturns));
+    fprintf('\nFinal Std Dev: %.2f', mean(finalReturns));
     fprintf('\n\n');
 end
