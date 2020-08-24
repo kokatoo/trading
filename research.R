ccc8089c64e83e74a27ef3e0506cc07894edd84a
kokatoo <kokatoo1948@gmail.com>
2020-08-24 18:07:11 +0800

    Clean up code and add code to org file

--- research.R
+++ research.R
@@ -1,136 +1,66 @@
 library(quantmod)
-library(RColorBrewer)
 library(gplots)
-library(lubridate)
 library(sm)
-library(reshape2)
 library(vioplot)
+library(reshape2)
+library(lubridate)
+library(RColorBrewer)
 
-##---- SPY
-getSymbols.yahoo(
-  Symbols = c("SPY", "EEM", "IWM"),
-  env = ".GlobalEnv",
-  from = "1995-01-01",
-  to = "2020-08-20"
-)
+## ---- only run once
+if (FALSE) {
+  getSymbols.yahoo(
+    Symbols = c("SPY", "EEM", "IWM"),
+    env = ".GlobalEnv",
+    from = "1995-01-01",
+    to = "2020-08-20"
+  )
 
+  data.all.SPY <- clean_data(SPY)
+  data.all.EEM <- clean_data(EEM)
+  data.all.IWM <- clean_data(IWM)
+
+  saveRDS(data.all.SPY, file = "./data/spy.rds")
+  saveRDS(data.all.EEM, file = "./data/eem.rds")
+  saveRDS(data.all.IWM, file = "./data/iwm.rds")
+}
+
+##---- Functions Definitions
 clean_data <- function(data) {
+  header <- names(data)
 
-  header  <- names(data)
-
+  # remove symbol name from header
   split_data <- strsplit(header, ".", fixed = TRUE)
   colnames(data) <- sapply(split_data, "[", length(split_data[[1]]))
 
   return(data)
 }
 
-myplot <- function(symbol, data) {
+price_plot <- function(symbol, data, col = "black") {
   plot(
     data,
     xlab = "Date",
     ylab = "Price",
+    col = col,
     main = paste(
       symbol,
-      " Price from ",
-      start(data),
-      " to ",
-      end(data)
+      " Price"
     )
   )
 }
 
+summary_plot <- function(symbol, summary_rets, col, subs) {
+  header <- c("Min.", "Max.", "Median", "Mean")
+  data <- data.frame(Month = summary_rets$Month, summary_rets[, symbol])
 
-data.SPY <- clean_data(SPY)
-data.EEM <- clean_data(EEM)
-data.IWM <- clean_data(IWM)
-
-saveRDS(data.SPY, file = "./data/spy.rds")
-saveRDS(data.EEM, file = "./data/eem.rds")
-saveRDS(data.IWM, file = "./data/iwm.rds")
-
-data.SPY <- readRDS("./data/spy.rds")
-data.EEM <- readRDS("./data/eem.rds")
-data.IWM <- readRDS("./data/iwm.rds")
-
-## --- Plot all series
-png(file = "./images/plot1.png", width = 1000)
-layout(matrix(c(1, 1, 2, 3), 2, 2, byrow = TRUE))
-myplot("SPY", data.SPY)
-myplot("EEM", data.EEM)
-myplot("IWM", data.IWM)
-dev.off()
-
-## --- Plot series with common dates
-first_date <- as.Date(max(sapply(list(data.SPY, data.EEM, data.IWM), function(x) index(first(x)))))
-
-data.SPY <- data.SPY[index(data.SPY) >= first_date]
-data.EEM <- data.EEM[index(data.EEM) >= first_date]
-data.IWM <- data.IWM[index(data.IWM) >= first_date]
-
-opar <- par(no.readonly = TRUE)
-png(file = "./images/plot2.png", width = 1000)
-par(mfrow = c(3, 1))
-myplot("SPY", data.SPY)
-myplot("EEM", data.EEM)
-myplot("IWM", data.IWM)
-par(opar)
-dev.off()
-
-## -- Plot all series in 1 plot
-data <- cbind(
-  data.SPY$Adjusted / as.numeric(data.SPY$Adjusted[1]),
-  data.EEM$Adjusted / as.numeric(data.EEM$Adjusted[1]),
-  data.IWM$Adjusted / as.numeric(data.IWM$Adjusted[1])
-)
-names(data) <- c("SPY", "EEM", "IWM")
-
-png(file = "./images/plot3.png", width = 1000)
-plot(
-  data,
-  main = "SPY vs EEM vs IWM",
-  legend.loc = "topleft"
-)
-dev.off()
-
-## --- Barplots
-
-log_rets <- diff(log(data), lag = 1)
-log_rets <- log_rets[-1]
-
-
-log_rets <- cbind(log_rets, month = month(index(log_rets)))
-
-aggregate_month <- function(symbol, log_rets) {
-  as.data.frame(aggregate(log_rets[, symbol],
-    by = list(log_rets$month),
-    FUN = summary
-  ) * 252)
-}
-
-mybarplots <- function(symbol, log_rets) {
-
-  log_rets_summary <- aggregate_month(symbol, log_rets)
-
-  log_rets_summary <- cbind(log_rets_summary, month = as.numeric(row.names(log_rets_summary)))
-  log_rets_summary <- log_rets_summary[order(log_rets_summary$month), ]
-
-  col_names <- c("Min.", "Max.", "Median", "Mean")
-
-  opar <- par(no.readonly = TRUE)
   par(mfrow = c(2, 2))
-
-  for (col in col_names) {
+  par(oma = c(oma = c(1, 1, 2, 1)))
+  for (col_name in header) {
     barplot(
-      log_rets_summary[, col] * 100,
-      names.arg = month.abb,
-      ylab = paste(col, "Returns (%)"),
+      data[, col_name] * 100,
+      ylab = paste(col_name, "Returns (%)"),
       yaxt = "n",
-      main = paste0(symbol, " Annual '", col, "' Returns"),
-      sub = paste(
-          index(first(log_rets)),
-          "to",
-          index(last(log_rets))
-        )
+      col = col,
+      main = col_name
     )
     axis(
       side = 2,
@@ -138,88 +68,195 @@ mybarplots <- function(symbol, log_rets) {
       labels = formatC(axTicks(2), format = "d", big.mark = ",")
     )
   }
-
-  par(opar)
+  mtext(paste(symbol, "Annual Return (%)"),
+    outer = TRUE,
+    font = 2,
+    cex = 1.3
+  )
+  mtext(
+    subs,
+    side = 1,
+    outer = TRUE,
+    cex = 0.8
+  )
 }
 
-png(file = "./images/plot4.png", width = 1000)
-mybarplots("SPY", log_rets)
-dev.off()
+summary_grp_plot <- function(summary_rets, summary_sds, symbols, colors, subs) {
 
-png(file = "./images/plot5.png", width = 1000)
-mybarplots("EEM", log_rets)
-dev.off()
+  header <- c("Min.", "Max.", "Median", "Mean")
 
-png(file = "./images/plot6.png", width = 1000)
-mybarplots("IWM", log_rets)
-dev.off()
+  par(mfrow = c(2, 2))
+  par(oma = c(oma = c(2, 1, 2, 1)))
 
-mygroupbarplots <- function(col, log_rets) {
+  for (col_name in header) {
+    data <- sapply(symbols, function(symbol) {
+      summary_rets[, symbol][, col_name] * 100
+    })
+    data <- t(data)
 
-  symbols <- c("SPY", "EEM", "IWM")
-  data <- aggregate_month(symbols[1], log_rets)
-  month <- as.numeric(row.names(data))
+    if (col_name == "Mean") {
+      sds <- summary_sds[, symbol]
+    } else {
+      sds <- matrix(0, nrow = nrow(data), ncol = ncol(data))
+    }
 
-  data <- cbind(
-    SPY = data[, col],
-    EEM = aggregate_month(symbols[2], log_rets)[, col],
-    IWM = aggregate_month(symbols[3], log_rets)[, col]
+    barplot2(
+      data,
+      col = colors,
+      beside = TRUE,
+      legend = symbols,
+      names.arg = month.abb,
+      yaxt = "n",
+      ylab = paste(col_name, "Returns (%)"),
+      main = col_name,
+      plot.ci = TRUE,
+      ci.l = (data - 1.96 * sds),
+      ci.u = (data + 1.96 * sds)
+    )
+
+    axis(
+      side = 2,
+      at = axTicks(2),
+      labels = formatC(axTicks(2), format = "d", big.mark = ",")
+    )
+  }
+  mtext(
+    paste(paste(symbols, collapse = ", "), "Annual Return (%)"),
+    outer = TRUE,
+    font = 2,
+    cex = 1.3
   )
 
-  data <- data[order(month), ]
-  data <- t(data)
-
-  if (col == "Mean") {
-    sds <- as.data.frame(
-      aggregate(log_rets[, symbols],
-        by = list(log_rets$month),
-        FUN = sd
-      )
-    )
-    sds <- sds[order(as.numeric(row.names(sds))), ]
-    sds <- t(sds)
-  } else {
-    sds <- matrix(0, nrow = nrow(data), ncol = ncol(data))
-  }
-
-  barplot2(data * 100,
-    col = brewer.pal(3, "Set1"),
-    beside = TRUE,
-    legend = rownames(data),
-    names.arg = month.abb,
-    yaxt = "n",
-    ylab = paste("Median Returns (%)"),
-    main = paste0("Annual '", col, "' Returns"),
-    sub = paste(
-      index(first(log_rets)),
-      "to",
-      index(last(log_rets))
-    ),
-    plot.ci = TRUE,
-    ci.l = (data - 1.96 * sds) * 100,
-    ci.u = (data + 1.96 * sds) * 100
-    )
-
-  axis(side = 2,
-       at = axTicks(2),
-       labels = formatC(axTicks(2), format = "d", big.mark = ","))
+  mtext(
+    subs,
+    side = 1,
+    outer = TRUE,
+    cex = 0.8
+  )
 }
 
+##---- Exploratory Analysis
+options(scipen = 100)
+opar <- par(no.readonly = TRUE)
+
+symbols <- c("SPY", "EEM", "IWM")
+colors <- brewer.pal(length(symbols), "Set1")
+
+data.all.SPY <- readRDS("./data/spy.rds")
+data.all.EEM <- readRDS("./data/eem.rds")
+data.all.IWM <- readRDS("./data/iwm.rds")
+
+first_date <- as.Date(
+  max(
+    sapply(
+      list(data.SPY, data.EEM, data.IWM),
+      function(x) index(first(x))
+    )
+  )
+)
+
+data.SPY <- data.all.SPY[index(data.all.SPY) >= first_date]
+data.EEM <- data.all.EEM[index(data.all.EEM) >= first_date]
+data.IWM <- data.all.IWM[index(data.all.IWM) >= first_date]
+
+class(data.SPY)
+head(data.SPY)
+
+data.normalized <- cbind(
+  data.SPY$Adjusted / as.numeric(data.SPY$Adjusted[1]),
+  data.EEM$Adjusted / as.numeric(data.EEM$Adjusted[1]),
+  data.IWM$Adjusted / as.numeric(data.IWM$Adjusted[1])
+)
+names(data.normalized) <- symbols
+
+## --- Log Returns
+log_rets <- diff(log(data.normalized), lag = 1)
+log_rets <- log_rets[-1]
+months <- month(log_rets, label = TRUE, abbr = TRUE)
+
+log_rets <- as.data.frame(log_rets)
+log_rets$Month <- months
+
+head(log_rets)
+str(log_rets)
+
+## --- Plot all datapoints
+png(file = "./images/plot1.png", width = 1000)
+
+par(oma = c(oma = c(1, 1, 2, 1)))
+layout(matrix(c(1, 1, 2, 3), 2, 2, byrow = TRUE))
+price_plot("SPY", data.all.SPY$Adjusted, col = colors[1])
+price_plot("EEM", data.all.EEM$Adjusted, col = colors[2])
+price_plot("IWM", data.all.IWM$Adjusted, col = colors[3])
+mtext("All Data Points", outer = TRUE)
+
+dev.off()
+
+## --- Plot matched datapoints
+
+png(file = "./images/plot2.png", width = 1000)
+
+par(opar)
+par(mfrow = c(3, 1))
+price_plot("SPY", data.SPY$Adjusted, col = colors[1])
+price_plot("EEM", data.EEM$Adjusted, col = colors[2])
+price_plot("IWM", data.IWM$Adjusted, col = colors[3])
+
+dev.off()
+
+## --- Normalize first price to 1
+png(file = "./images/plot3.png", width = 1000)
+
+par(opar)
+plot(
+  data.normalized,
+  col = colors,
+  main = "SPY vs EEM vs IWM",
+  legend.loc = "topleft"
+)
+
+dev.off()
+
+## --- Barplots
+summary_rets <- aggregate(
+  log_rets[symbols] * 252,
+  by = list(log_rets$Month),
+  FUN = summary
+)
+names(summary_rets)[1] <- "Month"
+
+summary_sds <- aggregate(
+  log_rets[symbols] * 252,
+  by = list(log_rets$Month),
+  FUN = sd
+)
+names(summary_sds)[1] <- "Month"
+
+subs <- paste(
+  row.names(first(log_rets)),
+  "/",
+  row.names(last(log_rets))
+)
+
+png(file = "./images/plot4.png", width = 1000)
+summary_plot("SPY", summary_rets, colors[1], subs)
+dev.off()
+png(file = "./images/plot5.png", width = 1000)
+summary_plot("EEM", summary_rets, colors[2], subs)
+dev.off()
+png(file = "./images/plot6.png", width = 1000)
+summary_plot("IWM", summary_rets, colors[3], subs)
+dev.off()
 
 png(file = "./images/plot7.png", width = 1000)
-par(mfrow = c(2, 2))
-mygroupbarplots("Min.", log_rets)
-mygroupbarplots("Max.", log_rets)
-mygroupbarplots("Median", log_rets)
-mygroupbarplots("Mean", log_rets)
+
+summary_grp_plot(summary_rets, summary_sds, symbols, colors, subs)
+
 dev.off()
 
 par(opar)
 
 ## --- Histograms
-
-symbols <- c("SPY", "EEM", "IWM")
-colors <- brewer.pal(length(symbols), "Set1")
 
 png(file = "./images/plot8.png", width = 1000)
 
@@ -245,14 +282,9 @@ for (i in seq_along(symbols)) {
 
 dev.off()
 
+## --- sm Plot
 log_rets <- as.data.frame(log_rets)
 log_rets$month <- factor(log_rets$month)
-log_ann_rets <- as.data.frame(log_rets)
-
-log_ann_rets$SPY <- log_rets$SPY * 252
-log_ann_rets$EEM <- log_rets$EEM * 252
-log_ann_rets$IWM <- log_rets$IWM * 252
-
 
 par(mfrow = c(1, 1))
 data.melt <- melt(log_rets, id = c("month"))
@@ -265,6 +297,7 @@ title(main = "Return Distribution by Symbol")
 legend("topright", levels(data.melt$variable), fill = c(2:(1 + length(levels(data.melt$variable)))))
 dev.off()
 
+## -- Box Plots
 png(file = "./images/plot10.png", width = 1000)
 boxplot(ret ~ symbol,
         data = data.melt,
@@ -275,6 +308,7 @@ boxplot(ret ~ symbol,
         notch = TRUE)
 dev.off()
 
+##-- No Outliers
 png(file = "./images/plot11.png", width = 1000)
 boxplot(ret ~ symbol,
   data = data.melt,
@@ -287,6 +321,7 @@ boxplot(ret ~ symbol,
 )
 dev.off()
 
+## -- Violin Plots
 png(file = "./images/plot12.png", width = 1000)
 vioplot(data.melt$ret[data.melt$symbol == "SPY"],
   data.melt$ret[data.melt$symbol == "EEM"],
